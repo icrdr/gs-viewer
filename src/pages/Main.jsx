1d4aa86dc4dbe6fe7fd90ff92259a0dab360da7c
icrdr <icrdr2010@gmail.com>
2019-09-27 15:18:42 +0800

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+function Thing() {
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.y += 0.01))
+  return (
+    <group ref={ref}>
+      <mesh
+        onClick={e => console.log('click')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        <octahedronGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" color="peachpuff" opacity={0.5} transparent />
+      </mesh>
+    </group>
+  )
+}
 
 export default function Main() {
   return (
-    <div>
-      主体
-    </div>
+    <Canvas style={{ height: '500px' }}>
+      <Thing />
+    </Canvas>
   )
 }
