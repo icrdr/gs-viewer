65d5fc43c6682101a788e514640e7d4f5f577466
icrdr <icrdr2010@gmail.com>
2019-09-29 15:10:20 +0800

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,27 +1,48 @@
-import React, { useRef } from 'react'
-import * as THREE from 'three'
-import { Canvas, useFrame } from 'react-three-fiber'
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
+import { useSpring, a } from "react-spring/three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+extend({ OrbitControls });
+
+function Controls() {
+  const constrol = useRef();
+  const { camera, gl } = useThree();
+  useFrame(() => {
+    constrol.current.update();
+  });
+  return <orbitControls ref={constrol} args={[camera, gl.domElement]} />;
+}
 
 function Thing() {
-  const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.01))
+  const cube = useRef();
+  const [isHover, setHover] = useState(false);
+  useFrame(() => {
+    cube.current.rotation.y += 0.01;
+  });
+  const props = useSpring({
+    color: isHover ? "hotpink" : "gray",
+    scale: isHover ? [2, 2, 2] : [1, 1, 1]
+  });
   return (
-    <group ref={ref}>
-      <mesh
-        onClick={e => console.log('click')}
-        onPointerOver={e => console.log('hover')}
-        onPointerOut={e => console.log('unhover')}>
-        <octahedronGeometry attach="geometry" />
-        <meshBasicMaterial attach="material" color="peachpuff" opacity={0.5} transparent />
-      </mesh>
-    </group>
-  )
+    <a.mesh
+      ref={cube}
+      onClick={e => console.log("click")}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}
+      scale={props.scale}
+    >
+      <boxGeometry attach="geometry" />
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
+  );
 }
 
 export default function Main() {
   return (
-    <Canvas style={{ height: '500px' }}>
+    <Canvas>
+      <ambientLight intensity={0.5} />
       <Thing />
+      <Controls />
     </Canvas>
-  )
+  );
 }
