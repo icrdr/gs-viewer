2825246503926958c810ef23bfb3bbe089b66221
icrdr <icrdr2010@gmail.com>
2019-09-30 14:45:14 +0800

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,20 +4,41 @@ import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useSpring, a } from "react-spring/three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { EquirectangularToCubeGenerator } from "three/examples/jsm/loaders/EquirectangularToCubeGenerator.js";
 import { PMREMGenerator } from "three/examples/jsm/pmrem/PMREMGenerator.js";
 import { PMREMCubeUVPacker } from "three/examples/jsm/pmrem/PMREMCubeUVPacker.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
 
 import Stats from "three/examples/jsm/libs/stats.module";
 import { Switch } from "antd";
-extend({ OrbitControls });
+extend({ OrbitControls, EffectComposer, ShaderPass, RenderPass });
 
 function Effect({ stats }) {
+  const { gl, scene, camera, size } = useThree();
+  const composer = useRef();
+  useEffect(() => {
+    composer.current.setSize(size.width, size.height);
+  }, [size]);
+
   useFrame(() => {
+    composer.current.render();
     stats.update();
   });
-  return null;
+  return (
+    <effectComposer ref={composer} args={[gl]}>
+      <renderPass attachArray="passes" args={[scene, camera]} />
+      <shaderPass
+        attachArray="passes"
+        args={[FXAAShader]}
+        uniforms-resolution-value={[1 / size.width, 1 / size.height]}
+        renderToScreen
+      />
+    </effectComposer>
+  );
 }
 
 function Controls() {
@@ -42,6 +63,8 @@ function Model({ url, envMap }) {
     new GLTFLoader().load(url, object => {
       object.scene.traverse(function(child) {
         if (child instanceof THREE.Mesh) {
+          console.log(child.material.normalScale.y);
+          child.material.normalScale.y = -1;
           setModels(prevState => [...prevState, child]);
         }
       });
@@ -49,33 +72,31 @@ function Model({ url, envMap }) {
     // eslint-disable-next-line
   }, []);
   const props = useSpring({
-    scale: isHover ? [2, 2, 2] : [1, 1, 1]
+    scale: isHover ? [1.2, 1.2, 1.2] : [1, 1, 1]
   });
   return models.map((model, index) => (
     <a.mesh
       key={index}
       onPointerOver={e => setHover(true)}
       onPointerOut={e => setHover(false)}
+      onUpdate={self => {
+        if (envMap) {
+          self.material.envMap = envMap;
+          self.material.needsUpdate = true;
+        }
+      }}
       scale={props.scale}
       geometry={model.geometry}
+      material={model.material}
       castShadow
       receiveShadow
-    >
-      <meshStandardMaterial
-        attach="material"
-        map={model.material.map}
-        metalnessMap={model.material.metalnessMap}
-        normalMap={model.material.normalMap}
-        roughnessMap={model.material.roughnessMap}
-        envMap={envMap}
-      />
-    </a.mesh>
+    />
   ));
 }
 
 function Plane({ color, envMap }) {
   const props = useSpring({
-    color: color ? "hotpink" : "gray",
+    color: color ? "hotpink" : "gray"
   });
   return (
     <mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
@@ -85,7 +106,6 @@ function Plane({ color, envMap }) {
         color={props.color}
         onUpdate={self => {
           if (envMap) {
-            console.log(envMap);
             self.needsUpdate = true;
           }
         }}
@@ -130,29 +150,31 @@ export default function Main() {
     <>
       <Switch checked={color} onChange={() => setColor(!color)}></Switch>
       <Canvas
-        className="ff"
-        camera={{ position: [0, 5, 10] }}
+        camera={{ position: [0, 2, 5] }}
         shadowMap
         onCreated={({ gl, scene }) => {
           const stats = new Stats();
           gl.domElement.parentElement.appendChild(stats.domElement);
           stats.domElement.style.position = "absolute";
-          console.log(gl.domElement);
           setStatsRef(stats);
-          new EXRLoader()
-            .setDataType(THREE.FloatType)
-            .load("/static/textures/piz_compressed.exr", function(texture) {
-              texture.minFilter = THREE.NearestFilter;
-              // texture.magFilter = THREE.NearestFilter;
-              texture.encoding = THREE.LinearEncoding;
+
+          gl.gammaOutput = true;
+          gl.gammaFactor = 2.2;
+
+          new RGBELoader()
+            .setDataType(THREE.UnsignedByteType)
+            .load("/static/textures/lakes_2k.hdr", function(texture) {
               const cubemapGenerator = new EquirectangularToCubeGenerator(
                 texture,
-                { resolution: 512, type: THREE.HalfFloatType }
+                { resolution: 1024 }
               );
+              cubemapGenerator.update(gl);
 
-              const cubeMapTexture = cubemapGenerator.update(gl);
-              const pmremGenerator = new PMREMGenerator(cubeMapTexture);
+              const pmremGenerator = new PMREMGenerator(
+                cubemapGenerator.renderTarget.texture
+              );
               pmremGenerator.update(gl);
+
               const pmremCubeUVPacker = new PMREMCubeUVPacker(
                 pmremGenerator.cubeLods
               );
@@ -161,22 +183,16 @@ export default function Main() {
               texture.dispose();
               pmremGenerator.dispose();
               pmremCubeUVPacker.dispose();
-
-              const exrBackground = cubemapGenerator.renderTarget;
-              const exrCubeRenderTarget = pmremCubeUVPacker.CubeUVRenderTarget;
-              console.log(exrBackground);
-              scene.background = exrBackground;
-              setTextureCube(exrCubeRenderTarget.texture);
-              console.log(exrCubeRenderTarget);
+              scene.background = cubemapGenerator.renderTarget;
+              setTextureCube(pmremCubeUVPacker.CubeUVRenderTarget.texture);
             });
         }}
       >
-        <ambientLight intensity={0.1} />
         <directionalLight position={[0, 5, 3]} castShadow />
         {/* <Cube /> */}
-        <Plane color={color} envMap={textureCube} />
+        {/* <Plane color={color} envMap={textureCube} /> */}
         <Controls />
-        <Model url="/static/models/meetMat/MeetMat.gltf" envMap={textureCube} />
+        <Model url="/static/models/sphere/test.gltf" envMap={textureCube} />
         <Effect stats={statsRef} />
       </Canvas>
     </>
