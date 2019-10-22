4bf9c87441d443bd1c07c9633416ea51f3e06ea6
icrdr <icrdr2010@gmail.com>
2019-10-23 07:29:55 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 
 // pages
 import Main from './pages/Main'
+import Sx from './pages/Sx'
 import Hello from './pages/Hello'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={Hello} />
       <Route path="/main" component={Main} />
+      <Route path="/sx" component={Sx} />
     </Switch>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,44 +1,19 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useSpring, a } from "react-spring/three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-import { EquirectangularToCubeGenerator } from "three/examples/jsm/loaders/EquirectangularToCubeGenerator.js";
-import { PMREMGenerator } from "three/examples/jsm/pmrem/PMREMGenerator.js";
-import { PMREMCubeUVPacker } from "three/examples/jsm/pmrem/PMREMCubeUVPacker.js";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
-import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
 
 import Stats from "three/examples/jsm/libs/stats.module";
-import { Switch } from "antd";
-extend({ OrbitControls, EffectComposer, ShaderPass, RenderPass });
+import { Button } from "antd";
+
+extend({ OrbitControls });
 
 function Effect({ stats }) {
-  const { gl, scene, camera, size } = useThree();
-  const composer = useRef();
-  useEffect(() => {
-    composer.current.setSize(size.width, size.height);
-  }, [size]);
-
-  useFrame(() => {
-    composer.current.render();
+  useFrame(({ gl, camera, scene }) => {
     stats.update();
   });
-  return (
-    <effectComposer ref={composer} args={[gl]}>
-      <renderPass attachArray="passes" args={[scene, camera]} />
-      <shaderPass
-        attachArray="passes"
-        args={[FXAAShader]}
-        uniforms-resolution-value={[1 / size.width, 1 / size.height]}
-        renderToScreen
-      />
-    </effectComposer>
-  );
+  return null;
 }
 
 function Controls() {
@@ -56,26 +31,26 @@ function Controls() {
   );
 }
 
-function Model({ url, envMap }) {
-  const [models, setModels] = useState([]);
+function Cube({ envMap }) {
+  const cube = useRef();
   const [isHover, setHover] = useState(false);
-  useEffect(() => {
-    new GLTFLoader().load(url, object => {
-      object.scene.traverse(function(child) {
-        if (child instanceof THREE.Mesh) {
-          child.material.normalScale.y = -1;
-          setModels(prevState => [...prevState, child]);
-        }
-      });
-    });
-    // eslint-disable-next-line
-  }, []);
-  const props = useSpring({
-    scale: isHover ? [1.2, 1.2, 1.2] : [1, 1, 1]
+  useFrame(() => {
+    cube.current.rotation.y += 0.01;
   });
-  return models.map((model, index) => (
+  const {gl} = useThree()
+  console.log(gl)
+  const colorV1 =
+    "#" + new THREE.Color("hotpink").convertSRGBToLinear().getHexString();
+  const colorV2 =
+    "#" + new THREE.Color("gray").convertSRGBToLinear().getHexString();
+  const props = useSpring({
+    color: isHover ? colorV1 : colorV2,
+    scale: isHover ? [2, 2, 2] : [1, 1, 1]
+  });
+  return (
     <a.mesh
-      key={index}
+      ref={cube}
+      onClick={e => console.log(e)}
       onPointerOver={e => setHover(true)}
       onPointerOut={e => setHover(false)}
       onUpdate={self => {
@@ -84,54 +59,6 @@ function Model({ url, envMap }) {
           self.material.needsUpdate = true;
         }
       }}
-      scale={props.scale}
-      geometry={model.geometry}
-      material={model.material}
-      castShadow
-      receiveShadow
-    />
-  ));
-}
-
-function Plane({ color, envMap }) {
-  const props = useSpring({
-    color: color ? "hotpink" : "gray"
-  });
-  return (
-    <mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[10, 10, 10]} />
-      <a.meshStandardMaterial
-        attach="material"
-        color={props.color}
-        onUpdate={self => {
-          if (envMap) {
-            self.needsUpdate = true;
-          }
-        }}
-        envMap={envMap}
-        roughness={0.5}
-        metalness={0.0}
-      />
-    </mesh>
-  );
-}
-
-function Cube() {
-  const cube = useRef();
-  const [isHover, setHover] = useState(false);
-  useFrame(() => {
-    cube.current.rotation.y += 0.01;
-  });
-  const props = useSpring({
-    color: isHover ? "hotpink" : "gray",
-    scale: isHover ? [2, 2, 2] : [1, 1, 1]
-  });
-  return (
-    <a.mesh
-      ref={cube}
-      onClick={e => console.log("click")}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}
       scale={props.scale}
       castShadow
     >
@@ -142,57 +69,68 @@ function Cube() {
 }
 
 export default function Main() {
-  const [textureCube, setTextureCube] = useState();
-  const [color, setColor] = useState(false);
-  const [statsRef, setStatsRef] = useState();
+  const [scene, setScene] = useState();
+  const [statsRefs, setStatsRefs] = useState([]);
+  const [objs, setObjs] = useState([]);
   return (
     <>
-      <Switch checked={color} onChange={() => setColor(!color)}></Switch>
+      <Button
+        onClick={() => {
+          const obj = {
+            geometry: new THREE.BoxBufferGeometry(),
+            material: new THREE.MeshStandardMaterial()
+          };
+          setObjs(prevState => [...prevState, obj]);
+        }}
+      ></Button>
       <Canvas
-        camera={{ position: [0, 2, 5] }}
-        shadowMap
+        style={{ height: "50%" }}
+        camera={{ position: [5, 1, 0] }}
+        onCreated={({ gl, scene }) => {
+          console.log(scene)
+          setScene(scene)
+          const stats = new Stats();
+          gl.domElement.parentElement.appendChild(stats.domElement);
+          stats.domElement.style.position = "absolute";
+          setStatsRefs(prevState => [...prevState, stats]);
+
+          gl.gammaOutput = true;
+          gl.gammaFactor = 2.2;
+          gl.physicallyCorrectLights = true;
+        }}
+      >
+        <directionalLight intensity={3} position={[0, 5, 3]} />
+        <Controls />
+        <Cube />
+        {objs.map((obj, index) => {
+          return (
+            <mesh
+              key={index}
+              position={[index,0,0]}
+              geometry={obj.geometry}
+              material={obj.material}
+            ></mesh>
+          );
+        })}
+        <Effect stats={statsRefs[0]} />
+      </Canvas>
+      <Canvas
+        style={{ height: "50%" }}
+        camera={{ position: [0, 1, 5] }}
         onCreated={({ gl, scene }) => {
           const stats = new Stats();
           gl.domElement.parentElement.appendChild(stats.domElement);
           stats.domElement.style.position = "absolute";
-          setStatsRef(stats);
+          setStatsRefs(prevState => [...prevState, stats]);
 
           gl.gammaOutput = true;
           gl.gammaFactor = 2.2;
-
-          new RGBELoader()
-            .setDataType(THREE.UnsignedByteType)
-            .load("/static/textures/leadenhall_market_1k.hdr", function(texture) {
-              const cubemapGenerator = new EquirectangularToCubeGenerator(
-                texture,
-                { resolution: 1024 }
-              );
-              cubemapGenerator.update(gl);
-
-              const pmremGenerator = new PMREMGenerator(
-                cubemapGenerator.renderTarget.texture
-              );
-              pmremGenerator.update(gl);
-
-              const pmremCubeUVPacker = new PMREMCubeUVPacker(
-                pmremGenerator.cubeLods
-              );
-              pmremCubeUVPacker.update(gl);
-
-              texture.dispose();
-              pmremGenerator.dispose();
-              pmremCubeUVPacker.dispose();
-              scene.background = cubemapGenerator.renderTarget;
-              setTextureCube(pmremCubeUVPacker.CubeUVRenderTarget.texture);
-            });
+          gl.physicallyCorrectLights = true;
         }}
       >
-        <directionalLight position={[0, 5, 3]} castShadow />
-        {/* <Cube /> */}
-        {/* <Plane color={color} envMap={textureCube} /> */}
         <Controls />
-        <Model url="/static/models/sphere/test.gltf" envMap={textureCube} />
-        <Effect stats={statsRef} />
+        {scene && <primitive object={scene}/>}
+        <Effect stats={statsRefs[1]} />
       </Canvas>
     </>
   );
